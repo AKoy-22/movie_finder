c892e8225860ebe91bb04bd74f0f7bd702e89cb4
Akiko Koyama <akiyama7584@gmail.com>
2023-08-28 15:25:57 -0700

    updated movies db api

--- index.js
+++ index.js
@@ -16,7 +16,25 @@ app.get('/home', (req, res)=>{
     res.render('index', {message:"This is the home page"})
 })
 
+app.get('/', (req,res)=>{
+    const url = 'https://api.themoviedb.org/3/account/20363388';
+    const options ={
+        method:'GET',
+        headers:{
+            accept: 'application/json',
+            Authorization:'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMzE2NzRiYjA3ODUxOGYwZDI5NjczOWU2ODkxZmY3OCIsInN1YiI6IjY0ZWJmYTU2YzNjODkxMDBlMzYxMmNmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Hb7VHFB0oSPNbjEbBekaeIFLySCk1L9nPCczryaO1gU',
+        }
+    };
+
+    fetch(url, options)
+    .then(response=>response.json())
+    .then(json=> res.json(json))
+    .catch(err=>res.status(500).json({error:'An error occured'}));
+})
+
 //Start server
 app.listen(PORT, ()=>{
     console.log(`Server is running of port ${PORT}`);
 })
+
+
